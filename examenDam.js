6a436d1147d4644b82bbb9aa9682f01c1a163eef
SerraSilvia <silvia.serra.7e6@itb.cat>
2025-01-13 10:37:11 +0100

    add exercicis

---
+++ examenDam.js
@@ -0,0 +1,29 @@
+function cambiaContenido() {
+
+    let titulo = document.getElementsByTagName("h1")[0];
+    titulo.textContent = "Título actualizado";
+
+    let parrafo = document.getElementsByTagName("p")[0];
+    parrafo.textContent = "Texto actualizado";
+
+}
+
+cambiaContenido();
+
+function cambiaImagen(){
+    let imagen = document.getElementById("descripcion");
+    imagen.src = "gato-173700-1024x576.jpeg";
+
+    let parrafoImg = document.getElementById("descripcion");
+    parrafoImg.textContent = "Imagen actualizada";
+}
+
+cambiaImagen();
+
+function anadeElementos(){
+    let nuevoP = document.getElementById("contenedor");
+    nuevoP.
+
+}
+
+
