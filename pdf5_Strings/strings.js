9c7969aa704f52446989c3c1fff00bc582114e01
SerraSilvia <silvia.serra.7e6@itb.cat>
2025-01-27 10:04:36 +0100

    strins ex2

--- pdf5_Strings/strings.js
+++ pdf5_Strings/strings.js
@@ -1,16 +1,18 @@
-function obtenStringEmpiezaConAs(string){
+function obtenStringEmpiezaConAs(string) {
 
     let contiene = string.toUpperCase();
 
-    if(contiene.startsWith("AS")){
-        document.querySelector("#contieneAs").textContent += string + "\n";
-        
-    } else {
+    if (contiene.startsWith("AS")) {
+
+    } else if (string === '') {
+        console.log("''");
+    }
+    else {
         string = 'AS' + string;
-        document.querySelector("#contieneAs").textContent += string + "\n";
     }
 
     console.log(string);
+    document.querySelector("#contieneAs").textContent += string;
     document.querySelector("#contieneAs").style.color = "blue";
 
 }
@@ -18,3 +20,20 @@ function obtenStringEmpiezaConAs(string){
 obtenStringEmpiezaConAs('ASTerisco');
 obtenStringEmpiezaConAs('TEroide');
 obtenStringEmpiezaConAs('');
+
+function eliminaCarracterPosicion(string, number) {
+
+    let longitud = string.length;
+
+    if (longitud < number) {
+        string = string.slice(0, number) + string.slice(number + 1);
+
+    }
+    console.log(string);
+    document.querySelector("#longitudPalabra").textContent += string;
+    document.querySelector("#longitudPalabra").style.color = "blue";
+
+}
+
+eliminaCarracterPosicion('murcielago', 3);
+eliminaCarracterPosicion('rinoceronte', 12);
