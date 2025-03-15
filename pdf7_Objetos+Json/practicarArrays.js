ebeb0dd66f2f9491bd7b00fbef594634de61b3d8
Silvia <silvia.serra.7e6@itb.cat>
2025-03-15 19:53:00 +0100

    add pdf7 practicar

---
+++ pdf7_Objetos+Json/practicarArrays.js
@@ -0,0 +1,12 @@
+/*Ejercicios para Practicar Arrays
+Crear un array con 5 números y obtener la suma de todos los elementos.
+Crear un array de cadenas de texto y convertir todas las cadenas a mayúsculas.
+Eliminar el primer y último elemento de un array de números.
+Crear un array de números y obtener el valor más alto y más bajo.
+Eliminar todos los valores duplicados en un array de números.
+Verificar si un número existe dentro de un array.
+Crear un array de números y ordenar los números de menor a mayor.
+Concatenar dos arrays en uno solo.
+Invertir el orden de los elementos de un array.
+Contar cuántas veces aparece un valor específico dentro de un array.
+*/
---
+++ pdf7_Objetos+Json/practicarSet.js
@@ -0,0 +1,11 @@
+/* Ejercicios para Practicar Set
+Crear un Set a partir de un array con elementos repetidos.
+Agregar un nuevo valor a un Set y luego verificar si existe ese valor.
+Eliminar un valor específico de un Set.
+Comprobar si un Set contiene un valor específico.
+Crear un Set con números y verificar el tamaño del Set.
+Unir dos Set de números y crear un nuevo Set con los valores combinados.
+Convertir un Set a un array.
+Eliminar todos los elementos de un Set (vaciar el Set).
+Comparar dos Set para verificar si son iguales (es decir, tienen los mismos valores).
+Filtrar los elementos de un Set que sean mayores a un número específico y crear un nuevo Set.*/
